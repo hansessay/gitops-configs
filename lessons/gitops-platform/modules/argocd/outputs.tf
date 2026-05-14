41084ae2871df6f89f01ea0d0b95758578568a57
Cheikh <hansessay@hotmail.com>
2026-05-14 19:17:11 +0200

    cleanup: remove lesson training files

--- lessons/gitops-platform/modules/argocd/outputs.tf
+++
@@ -1,4 +0,0 @@
-output "status" {
-  description = "ArgoCD Helm release status."
-  value       = helm_release.argocd.status
-}
--- lessons/gitops-platform/modules/argocd/variables.tf
+++
@@ -1,9 +0,0 @@
-variable "namespace" {
-  description = "Namespace for ArgoCD."
-  type        = string
-}
-
-variable "environment" {
-  description = "Environment name."
-  type        = string
-}
